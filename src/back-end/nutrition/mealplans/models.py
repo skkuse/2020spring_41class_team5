c6ed75df919b5e8f87c09a65c9889e33fd5df9cd
YeongL <34955920+YeongL@users.noreply.github.com>
2020-06-11 11:14:49 +0900

    Update models.py

--- src/back-end/nutrition/mealplans/models.py
+++ src/back-end/nutrition/mealplans/models.py
@@ -6,8 +6,14 @@ from django.utils import timezone
 class MealPlan(models.Model):
     id = models.AutoField(primary_key=True)
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(default="None", max_length=100)
+    mealName = models.CharField(default="None", max_length=100)
+    mealCalories = models.IntegerField(default=0, max_length=10)
+    mealCarbonhydrate = models.IntegerField(default=0, max_length=10)
+    mealProtein = models.IntegerField(default=0, max_length=10)
+    mealFat = models.IntegerField(default=0, max_length=10)
+    mealMineral = models.IntegerField(default=0, max_length=10)
+    mealVitaminC = models.IntegerField(default=0, max_length=10)
     # delcreateddate = models.DateTimeField(auto_now_add=True)
-    mealType = models.CharField(default="None", max_length=100)
+    #mealType = models.CharField(default="None", max_length=100)
     # User = To whom belongs this delivery (=> cross reference to items)
     # items = reference to Shopping list and make a list out of it
